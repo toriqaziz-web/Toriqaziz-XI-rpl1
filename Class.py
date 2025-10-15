37711c200c9e2b6b49904c786a8a140ac8d3cf32
toriqaziz-web <famsnick@gmail.com>
2025-10-15 14:49:58 +0700

    Add files via upload

---
+++ Class.py
@@ -0,0 +1,27 @@
+class Sekolah:
+  pass
+
+sekolah1=Sekolah()
+sekolah1.nama= "SMK PGRI 35 SOLOKAN JERUK"
+sekolah1.alamat= "JL RHO Kosasih no 90 cibodas"
+
+sekolah2=Sekolah() 
+sekolah2.nama= "SMKN 9 Bandung"
+sekolah2.alamat= "JL Soekarno-hatta no 10"
+
+
+print(sekolah1.nama)
+print(sekolah1.alamat)
+print(sekolah2.nama)
+print(sekolah2.alamat)
+
+
+
+class Siswa:
+  pass
+  
+siswa1=Siswa()
+siswa2=Siswa()
+
+print(type(siswa1))
+print(type(siswa2)) 
