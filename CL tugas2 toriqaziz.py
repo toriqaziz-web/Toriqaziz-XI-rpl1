6d51a34f8d0eaa47b1a2eb66745cf39461aeb455
toriqaziz-web <famsnick@gmail.com>
2025-11-11 13:12:40 +0700

    Add files via upload

---
+++ CL tugas2 toriqaziz.py
@@ -0,0 +1,32 @@
+class alatmusik:  
+
+    nama1 = "Piano"
+    jenis1 = "Tekan"
+    bahan1 = "Kayu dan logam"
+    warna1 = "Hitam"
+    asal1= "Italia"
+
+
+    nama2 = "Drum"
+    jenis2 = "Pukul"
+    bahan2 = "Kayu dan plastik"
+    warna2 = "Merah"
+    asal2 = "Afrika"
+
+alatmusik1 = alatmusik()
+alatmusik2 = alatmusik()
+
+
+print(alatmusik1.nama1)
+print(alatmusik1.jenis1)
+print(alatmusik1.bahan1)
+print(alatmusik1.warna1)
+print(alatmusik1.asal1)
+
+print()
+
+print(alatmusik2.nama2)
+print(alatmusik2.jenis2)
+print(alatmusik2.bahan2)
+print(alatmusik2.warna2)
+print(alatmusik2.asal2)
